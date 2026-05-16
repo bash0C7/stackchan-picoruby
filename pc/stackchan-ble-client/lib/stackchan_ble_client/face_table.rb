124e468fd650db77c451fe0de27629f2b5138dfe
bash0C7 <ksb.4038.nullpointer+github@gmail.com>
2026-05-17 02:27:20 +0900

    feat(ble-client): add FaceTable + LedColorTable constants

--- pc/stackchan-ble-client/lib/stackchan_ble_client/face_table.rb
+++ pc/stackchan-ble-client/lib/stackchan_ble_client/face_table.rb
@@ -1,1 +1,10 @@
-module StackchanBleClient; end
+module StackchanBleClient
+  module FaceTable
+    FACE_INDICES = {
+      neutral:   "0",
+      smile:     "1",
+      joy:       "2",
+      surprised: "3",
+    }.freeze
+  end
+end
