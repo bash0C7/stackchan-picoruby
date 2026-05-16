07834b54d3ef0751ebbeb7560dad97ece45f5e17
bash0C7 <ksb.4038.nullpointer+github@gmail.com>
2026-05-17 02:42:01 +0900

    chore: delete pc/stackchan-protocol/ gem (superseded by stackchan-ble-client + lib/deploy/picomodem)

--- pc/stackchan-protocol/lib/stackchan_protocol.rb
+++
@@ -1,3 +0,0 @@
-require_relative "stackchan_protocol/version"
-require_relative "stackchan_protocol/face_table"
-require_relative "stackchan_protocol/client"
--- pc/stackchan-protocol/test/test_helper.rb
+++
@@ -1,6 +0,0 @@
-$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
-$LOAD_PATH.unshift File.expand_path(".", __dir__)
-
-require "test/unit"
-require "fake_uart"
-require "stringio"
