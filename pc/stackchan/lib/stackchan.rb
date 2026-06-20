b82bc21cbc17604aea47aebfc0bec98d292f18eb
bash0C7 <ksb.4038.nullpointer+github@gmail.com>
2026-06-20 14:16:20 +0900

    feat(stackchan): scaffold unified pc/stackchan package

---
+++ pc/stackchan/lib/stackchan.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+require_relative "stackchan/version"
+
+module Stackchan
+end
---
+++ pc/stackchan/lib/stackchan/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Stackchan
+  VERSION = "0.1.0"
+end
