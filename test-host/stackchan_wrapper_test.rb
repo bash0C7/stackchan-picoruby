802983dd4ddbbbc87b159d85ddc956f029220288
bash0C7 <ksb.4038.nullpointer+github@gmail.com>
2026-08-31 22:34:05 +0900

    refactor(pc): make the stackchan wrapper attach-only

---
+++ test-host/stackchan_wrapper_test.rb
@@ -0,0 +1,50 @@
+require 'test/unit'
+require 'fileutils'
+require 'open3'
+
+# The wrapper's whole remaining job is to exec the PicoRuby CLI with the right
+# argv. A fake VM records what it was handed, so this needs neither PicoRuby
+# nor a running daemon.
+class StackchanWrapperTest < Test::Unit::TestCase
+  ROOT = File.expand_path("..", __dir__)
+  DIR  = "/tmp/stackchan_wrapper_test_#{Process.pid}"
+
+  def setup
+    FileUtils.mkdir_p(DIR)
+    @fake_vm = File.join(DIR, "fake_picoruby")
+    File.write(@fake_vm, "#!/bin/sh\nfor a in \"$@\"; do echo \"$a\"; done\n")
+    File.chmod(0o755, @fake_vm)
+  end
+
+  def teardown
+    FileUtils.rm_rf(DIR)
+  end
+
+  def run_wrapper(*args)
+    out, _err, status = Open3.capture3(
+      { "STACKCHAN_PICORUBY" => @fake_vm, "STACKCHAN_ROOT" => ROOT, "STACKCHAN_PORT" => "8787" },
+      File.join(ROOT, "pc/stackchan-pico/bin/stackchan"), *args
+    )
+    [out.lines.map(&:chomp), status]
+  end
+
+  def test_passes_root_port_and_the_verb_through_to_the_cli
+    argv, status = run_wrapper("face", "joy")
+    assert_equal ["#{ROOT}/pc/stackchan-pico/app/boot_cli.rb", ROOT, "8787", "face", "joy"], argv
+    assert_true status.success?
+  end
+
+  def test_does_not_start_anything_itself
+    argv, = run_wrapper("status")
+    assert_equal 1, argv.count { |a| a.end_with?("boot_cli.rb") }
+    refute_match(/run_with_watchdog/, argv.join(" "))
+  end
+
+  # Everything that used to guess about resident processes is gone.
+  def test_the_wrapper_no_longer_contains_probe_or_kill_machinery
+    src = File.read(File.join(ROOT, "pc/stackchan-pico/bin/stackchan"))
+    %w[nc\ -z probe_daemon probe_sidecar ensure_daemon ensure_sidecar pkill run_with_watchdog].each do |gone|
+      assert_false src.include?(gone), "wrapper still mentions #{gone}"
+    end
+  end
+end
